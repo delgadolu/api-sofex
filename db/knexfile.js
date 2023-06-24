61f4fd22b05f846c7f53b4be880684f2be2eb425
Luisana <luisanaa50@gmail.com>
2023-06-24 12:09:20 -0400

    first commit

--- db/knexfile.js
+++ db/knexfile.js
@@ -6,23 +6,23 @@
 
 const { dev } = require("objection");
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: {
-      database: "db_sofex",
-      user: "postgres",
-      password: "123456",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./seeds",
-    },
-    ...dev,
-  },
+	development: {
+		client: "postgresql",
+		connection: {
+			database: process.env.NAME_DB,
+			user: process.env.USER_DB,
+			password: process.env.PASS_DB,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+		seeds: {
+			directory: "./seeds",
+		},
+		...dev,
+	},
 };
