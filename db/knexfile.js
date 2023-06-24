e295bdefb577cda67c196a2e4b4b8b19f29d844b
Luisana <luisanaa50@gmail.com>
2023-06-24 11:55:44 -0400

    first commit

---
+++ db/knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+const { dev } = require("objection");
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "db_sofex",
+      user: "postgres",
+      password: "123456",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+    ...dev,
+  },
+};
